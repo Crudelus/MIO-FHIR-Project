54ac9e383940d81c74ebe9449a00c3005eff6bbb
Daniel <danielrehmann@web.de>
2014-11-20 17:26:29 +0100

    Initial commit, Maven pom and structure created

---
+++ src/main/java/de/uniluebeck/imi/mio/FhirProject/App.java
@@ -0,0 +1,13 @@
+package de.uniluebeck.imi.mio.FhirProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
