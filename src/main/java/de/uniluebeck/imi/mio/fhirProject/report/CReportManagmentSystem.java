53c54a01e64ff8dc3dafa60bc5f66ca90be859cd
Loki24101988 <raphael.sues@mi.uni-luebeck.de>
2014-12-02 14:14:31 +0100

    CReportManagmentSystem updated

--- src/main/java/de/uniluebeck/imi/mio/fhirProject/report/CReportManagmentSystem.java
+++ src/main/java/de/uniluebeck/imi/mio/fhirProject/report/CReportManagmentSystem.java
@@ -5,52 +5,50 @@ import java.util.List;
 import ca.uhn.fhir.model.dstu.resource.DiagnosticReport;
 import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.client.IGenericClient;
 import de.uniluebeck.imi.mio.fhirProject.interfaces.IReport;
 
 public class CReportManagmentSystem implements IReport
 {
 
-	public List<DiagnosticReport> getAllPatientReports(IdDt patient) 
-	{
+	public List<DiagnosticReport> getAllPatientReports(IGenericClient client,
+			IdDt patient) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public MethodOutcome createNewReport()
-	{
+	public MethodOutcome createNewReport(IGenericClient client) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public DiagnosticReport getReport(IdDt report)
-	{
+	public DiagnosticReport getReport(IGenericClient client, IdDt report) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public boolean updateReport(IdDt report, DiagnosticReport newReport)
-	{
+	public boolean updateReport(IGenericClient client, IdDt report,
+			DiagnosticReport newReport) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public boolean deleteReport(IdDt report)
-	{
+	public boolean deleteReport(IGenericClient client, IdDt report) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public DiagnosticReport getVRead(IdDt report)
-	{
+	public DiagnosticReport getVRead(IGenericClient client, IdDt report) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public String getXMLString(IdDt report)
-	{
+	public String getXMLString(IGenericClient client, IdDt report) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 	
 	
 }
