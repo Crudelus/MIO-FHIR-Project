3a5d4241afe0563ca5cbfb567944990610ac1ef9
Loki24101988 <raphael.sues@mi.uni-luebeck.de>
2014-12-16 12:47:09 +0100

    blaaa

---
+++ src/main/java/de/uniluebeck/imi/mio/fhirProject/view/functions/AddReportFunctions.java
@@ -0,0 +1,37 @@
+package de.uniluebeck.imi.mio.fhirProject.view.functions;
+
+import ca.uhn.fhir.model.dstu.composite.CodeableConceptDt;
+import ca.uhn.fhir.model.dstu.resource.DiagnosticReport;
+import ca.uhn.fhir.model.dstu.valueset.DiagnosticReportStatusEnum;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import de.uniluebeck.imi.mio.fhirProject.App;
+import de.uniluebeck.imi.mio.fhirProject.report.CReportManagmentSystem;
+
+
+
+public class AddReportFunctions
+{
+	public static void uploadnewReport(String author, String date, String title ,String content, int status)
+	{
+		
+		MethodOutcome outcome = App.rms.createNewReport(App.ctx);
+		DiagnosticReport report = new DiagnosticReport();
+		report.setId(outcome.getId());
+		report.setName(new CodeableConceptDt("", title));
+		//SetStatus
+		switch(status)
+		{
+			case 1: report.setStatus(DiagnosticReportStatusEnum.AMENDED);break;
+			case 2: report.setStatus(DiagnosticReportStatusEnum.APPENDED);break;
+			case 3: report.setStatus(DiagnosticReportStatusEnum.CANCELLED);break;
+			case 4: report.setStatus(DiagnosticReportStatusEnum.CORRECTED);break;
+			case 5: report.setStatus(DiagnosticReportStatusEnum.ENTERED_IN_ERROR);break;
+			case 6: report.setStatus(DiagnosticReportStatusEnum.FINAL);break;
+			case 7: report.setStatus(DiagnosticReportStatusEnum.PARTIAL);break;
+			case 8: report.setStatus(DiagnosticReportStatusEnum.REGISTERED);break;
+			default: report.setStatus(DiagnosticReportStatusEnum.FINAL);break;
+		}
+		
+		
+	}
+}
--- src/main/java/de/uniluebeck/imi/mio/fhirProject/view/functions/MainWindowFunctions.java
+++ src/main/java/de/uniluebeck/imi/mio/fhirProject/view/functions/MainWindowFunctions.java
@@ -1,6 +1,5 @@
 package de.uniluebeck.imi.mio.fhirProject.view.functions;
 
-import javax.swing.JTable;
 
 public class MainWindowFunctions
 {
