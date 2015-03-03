fc80e6ac3be988e7596830f38c60a033d71ab737
Loki24101988 <raphael.sues@mi.uni-luebeck.de>
2015-03-03 13:41:28 +0100

    GetPatient getSpecialPatient(idDt pateintID) created

--- src/main/java/de/uniluebeck/imi/mio/fhirProject/App.java
+++ src/main/java/de/uniluebeck/imi/mio/fhirProject/App.java
@@ -7,6 +7,7 @@ import ca.uhn.fhir.model.dstu.valueset.DiagnosticReportStatusEnum;
 import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.rest.client.IGenericClient;
 import de.uniluebeck.imi.mio.fhirProject.devices.MIODeviceSystem;
+import de.uniluebeck.imi.mio.fhirProject.patientManagement.PatientManagementSystem;
 import de.uniluebeck.imi.mio.fhirProject.report.CReportManagmentSystem;
 import de.uniluebeck.imi.mio.fhirProject.report.DeviceCommunicationSystem;
 
@@ -19,6 +20,7 @@ public class App
 	public static final FhirContext ctx = new FhirContext();
 	public static final CReportManagmentSystem rms = new CReportManagmentSystem();
 	public static final DeviceCommunicationSystem dcs = new DeviceCommunicationSystem();
+	public static final PatientManagementSystem pms = new PatientManagementSystem(ctx.newRestfulGenericClient("http://fhirtest.uhn.ca/base"));
 	
     public static void main( String[] args )
     {
@@ -26,7 +28,6 @@ public class App
 
 	    // Server infrastructure:
 	    IGenericClient client = ctx.newRestfulGenericClient(serverBase);
-      
     }
     
 }
--- src/main/java/de/uniluebeck/imi/mio/fhirProject/clientApplication/GetPatient.java
+++ src/main/java/de/uniluebeck/imi/mio/fhirProject/clientApplication/GetPatient.java
@@ -1,6 +1,25 @@
 package de.uniluebeck.imi.mio.fhirProject.clientApplication;
 
+import java.util.List;
+
+import ca.uhn.fhir.model.dstu.resource.Patient;
+import ca.uhn.fhir.model.primitive.IdDt;
+import de.uniluebeck.imi.mio.fhirProject.App;
+
 public class GetPatient
 {
-
+	public static Patient getSpecialPatient(IdDt patientID)
+	{
+		List<Patient> erg = App.pms.getPatientList();
+		
+		for(Patient pat: erg)
+		{
+			if(pat.getId().getValue().compareTo(patientID.getValue()) == 0)
+			{
+				return pat;
+			}
+		}
+		
+		return null;
+	}
 }
