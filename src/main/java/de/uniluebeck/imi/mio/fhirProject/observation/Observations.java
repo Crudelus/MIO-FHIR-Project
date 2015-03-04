f4efbe2b8c9a0268f5e9926c2853228d5e6a848f
Loki24101988 <raphael.sues@mi.uni-luebeck.de>
2015-03-04 15:11:16 +0100

    editing Observations.getStations

--- src/main/java/de/uniluebeck/imi/mio/fhirProject/observation/Observations.java
+++ src/main/java/de/uniluebeck/imi/mio/fhirProject/observation/Observations.java
@@ -9,11 +9,14 @@ import de.uniluebeck.imi.mio.fhirProject.patientManagement.AdmissionParameters;
 import ca.uhn.fhir.model.dstu.composite.AddressDt;
 import ca.uhn.fhir.model.dstu.composite.HumanNameDt;
 import ca.uhn.fhir.model.dstu.composite.ResourceReferenceDt;
+import ca.uhn.fhir.model.dstu.resource.Organization;
 //import ca.uhn.fhir.model.dstu.resource.Claim.Patient;
 import ca.uhn.fhir.model.dstu.resource.Patient;
+import ca.uhn.fhir.model.dstu.resource.Practitioner;
 import ca.uhn.fhir.model.dstu.valueset.AdministrativeGenderCodesEnum;
 import ca.uhn.fhir.model.dstu.valueset.EncounterClassEnum;
 import ca.uhn.fhir.model.dstu.valueset.MaritalStatusCodesEnum;
+import ca.uhn.fhir.model.dstu.valueset.PractitionerRoleEnum;
 import ca.uhn.fhir.model.primitive.DateTimeDt;
 import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.rest.client.IGenericClient;
@@ -141,15 +144,161 @@ public class Observations implements IObservation {
 				 }
 			 } 
 			 
-			 for(AddressDt adress : p.getAddress()) { System.out.println(adress.getCountry().getValueAsString() == null); if(! (adress.getCountry().getValue() == null)) { vec.add(adress.getCountry().getValueAsString()); }else { vec.add(""); } if(!(adress.getCity().getValue()==null)) { vec.add(adress.getCity().getValueAsString()); } else { vec.add(""); } if(!(adress.getZip().getValue()==null)) { vec.add(adress.getZip().getValueAsString()); }else { vec.add(""); } if(!(adress.getLineFirstRep().getValueAsString()==null)) { vec.add(adress.getLineFirstRep().getValueAsString()); } else { vec.add(""); } } }
+			 for(AddressDt adress : p.getAddress()) 
+			 {
+				
+				 if( !(adress.getCountry().getValue() == null) )
+				 {
+					 vec.add(adress.getCountry().getValueAsString());
+				 }
+				 else
+				 {
+					 vec.add(""); 
+				 }
+				 
+				 if( !(adress.getCity().getValue()==null) )
+				 {
+					 vec.add(adress.getCity().getValueAsString()); 
+				 } 
+				 
+				 else
+				 {
+					 vec.add(""); 
+				 }
+				 
+				 if( !(adress.getZip().getValue()==null) )
+				 {
+					 vec.add(adress.getZip().getValueAsString());
+				 }
+				 else
+				 {
+					 vec.add(""); 
+				 } 
+				 
+				 if( !(adress.getLineFirstRep().getValueAsString()==null) ) 
+				 {
+					 
+					 vec.add(adress.getLineFirstRep().getValueAsString());
+					 
+				 }
+				 else
+				 {
+					 vec.add(""); 
+					 
+				 }
+				 
+			 }
+			 
+		 }
 		 
+		 return vec; 
 		 
-		 return vec; }
+	 }
 	
 	
 	public Vector<String> getStations()
 	{
+		Bundle organisations = client.search().forResource(Organization.class).execute();
+		Bundle practioniers = client.search().forResource(Practitioner.class).execute();
+		Bundle patients = client.search().forResource(Patient.class).execute();
+		List<Organization> list = organisations.getResources(Organization.class);
+		List<Practitioner> pracList = practioniers.getResources(Practitioner.class);
+		List<Patient> patList = patients.getResources(Patient.class);
 		
-		return null;
+		Vector<String> vec = new Vector<String>();
+		
+		for(Organization org : list)
+		{
+			
+			vec.add(org.getId().getValueAsString());
+			
+			if(org.getName().getValueAsString() != null)
+			{
+				vec.add(org.getName().getValueAsString());
+			}
+			else
+			{
+				vec.add("");
+			}
+			
+			/*
+			 * Alle Ärzte
+			 */
+			for(Practitioner prac : pracList)
+			{
+				boolean stillFilled = false;
+				int posInVec = 0;
+				String orgaString = prac.getOrganization().getReference().getValueAsString();
+				if((orgaString.compareTo(org.getName().getValueAsString()) == 0) && (prac.getRoleFirstRep().getValueAsEnum().contains(PractitionerRoleEnum.DOCTOR)))
+				{
+					if(!stillFilled)
+					{
+						stillFilled = true;
+						posInVec = vec.size()-1;
+						vec.add(prac.getName().getPrefixAsSingleString()+" "+prac.getName().getGivenAsSingleString()+", "+prac.getName().getFamilyAsSingleString()+";");
+					}else
+					{
+						String tmp = vec.get(posInVec);
+						tmp += prac.getName().getPrefixAsSingleString()+" "+prac.getName().getGivenAsSingleString()+", "+prac.getName().getFamilyAsSingleString()+";";
+						vec.set(posInVec, tmp);
+					}
+					
+				}
+			}
+			
+			//Ende der Ärzte
+			/*
+			 * Kommen wir nun zu den 
+			 * Krankenschwestern
+			 */
+			for(Practitioner prac : pracList)
+			{
+				boolean stillFilled = false;
+				int posInVec = 0;
+				String orgaString = prac.getOrganization().getReference().getValueAsString();
+				if((orgaString.compareTo(org.getName().getValueAsString()) == 0) && (prac.getRoleFirstRep().getValueAsEnum().contains(PractitionerRoleEnum.NURSE)))
+				{
+					if(!stillFilled)
+					{
+						stillFilled = true;
+						posInVec = vec.size()-1;
+						vec.add(prac.getName().getPrefixAsSingleString()+" "+prac.getName().getGivenAsSingleString()+", "+prac.getName().getFamilyAsSingleString()+";");
+					}else
+					{
+						String tmp = vec.get(posInVec);
+						tmp += prac.getName().getPrefixAsSingleString()+" "+prac.getName().getGivenAsSingleString()+", "+prac.getName().getFamilyAsSingleString()+";";
+						vec.set(posInVec, tmp);
+					}
+					
+				}
+			}
+			
+			//Ende der Krankenschwestern
+			
+			/*
+			 * Auf gehts zu den Patienten.
+			 */
+			for(Patient pat : patList)
+			{
+				boolean stillFilled = false;
+				int posInVec = 0;
+				if(pat.getManagingOrganization().getReference().getVersionIdPartAsLong() == org.getId().getVersionIdPartAsLong())
+				{
+					stillFilled = true;
+					posInVec = vec.size()-1;
+					vec.add(pat.getId()+"; ");
+				}else
+				{
+					String tmp = vec.get(posInVec);
+					tmp += pat.getId()+"; ";
+					vec.set(posInVec, tmp);
+				}
+			}
+			
+		
+		
+		}
+		
+		return vec;
 	}
 }
